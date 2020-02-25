7a220fa72c04741c4e3a784faf3235b686f61f50
flodrwho <flora10fauna@gmail.com>
2020-02-25 17:27:15 +1300

    Update cooking.sql

--- cooking.sql
+++ cooking.sql
@@ -5,7 +5,7 @@ CREATE TABLE Recepies (RID int NOT NULL identity primary key, RName VARCHAR(40) 
 
 CREATE TABLE Ingredients (IID int NOT NULL identity primary key, IName VARCHAR(40) not null, IAltNames VARCHAR(100), IEssential bit, IStored Varchar(15), ICalories float, IFat float, IProtein float, ICarbs float);
 
-CREATE TABLE RecepiesIngredients (RIID int NOT NULL identity primary key, RID int not null, foreign key (RID) references Recepies(RID), IID int not null, foreign key (IID) references Ingredients(IID));
+CREATE TABLE RecepiesIngredients (RIID int NOT NULL identity primary key, RID int not null, foreign key (RID) references Recepies(RID), IID int not null, foreign key (IID) references Ingredients(IID), IAmount float, IMeasurement Varchar(20));
 
 --ingredients for fritatta
 INSERT INTO Ingredients VALUES('butter', null, 1, 'fridge', 717, 81, 0.8, 0.1);
@@ -19,3 +19,10 @@ INSERT INTO Ingredients VALUES('capcicum', null, 0, 'fridge',  null, null, null,
 -- time data type hh:mm:ss within single quote marks
 INSERT INTO Recepies VALUES('fritata', null, 'main', 'egg', '0:20:0', '0:10:0', 6, 'fritata.txt');
 
+INSERT INTO Recepies VALUES(1,1, 50, 'grams');
+INSERT INTO Recepies VALUES(1,2, 4, null);
+INSERT INTO Recepies VALUES(1,3, 4, 'Tbsp');
+INSERT INTO Recepies VALUES(1,4, 0.25, 'cups');
+INSERT INTO Recepies VALUES(1,5, 3, null);
+INSERT INTO Recepies VALUES(1,6, 2, 'cups');
+INSERT INTO Recepies VALUES(1,7, 0.5, null);
